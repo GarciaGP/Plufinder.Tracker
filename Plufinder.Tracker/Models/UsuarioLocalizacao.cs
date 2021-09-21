c17578cd1e0606c983653055d6e50c153d6b25d3
GarciaGP <gabriel.garp@outlook.com>
2021-09-21 08:42:37 -0300

    Add project files.

---
+++ Plufinder.Tracker/Models/UsuarioLocalizacao.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plufinder.Tracker.Models
+{
+    class UsuarioLocalizacao
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("id_usuario")]
+        public int IdUsuario { get; set; }
+
+        [BsonElement("id_localizacao")]
+        public int IdLocalizacao { get; set; }
+    }
+}
---
+++ Plufinder.Tracker/Tracker.cs
@@ -0,0 +1,76 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using MongoDB.Driver;
+using Plufinder.Tracker.Models;
+using MongoDB.Bson;
+
+namespace Plufinder.Tracker
+{
+    public static class Tracker
+    {
+        [FunctionName("PlufinderTracker")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+
+        {
+            string responseMessage = "mongodb+srv://plufinderatlas:fiap123@plufindertracker.l6ath.mongodb.net/Plufinder?connect=replicaSet";
+            log.LogInformation("Inicializando Azure Function . . .");
+            int usuario = int.Parse(req.Query["usuario"]);
+            int localizacao = int.Parse(req.Query["localizacao"]);
+
+            log.LogInformation("Realizando conexão com o MongoDB . . .");
+            var client = new MongoClient("mongodb+srv://plufinderatlas:fiap123@plufindertracker.l6ath.mongodb.net/Plufinder?connect=replicaSet");
+            var session = client.StartSession();
+            var database = client.GetDatabase("Plufinder");
+            var collection = database.GetCollection<UsuarioLocalizacao>("UserLocation");
+
+            try
+            {
+                var filter = new FilterDefinitionBuilder<UsuarioLocalizacao>().Where(u => u.IdUsuario == usuario);
+                //var update = Builders<UsuarioLocalizacao>.Update.Set
+
+                log.LogInformation("Verificando se o usuário já está na base de localização . . .");
+                var usuarioBase = await collection.Find(Builders<UsuarioLocalizacao>.Filter.Eq("id_usuario", usuario)).FirstOrDefaultAsync();
+
+                session.StartTransaction();
+                if (usuarioBase != null)
+                {
+                    usuarioBase.IdLocalizacao = localizacao;
+                    log.LogInformation("Atualizando a localização do usuário . . .");
+                    await collection.FindOneAndReplaceAsync(filter, usuarioBase);
+                }
+                else
+                {
+                    log.LogInformation("Inserindo novo usuário nas localizações . . .");
+                    var novoUsuarioLocalizacao = new UsuarioLocalizacao
+                    {
+                        Id = new ObjectId(),
+                        IdUsuario = usuario,
+                        IdLocalizacao = localizacao
+                    };
+                    await collection.InsertOneAsync(novoUsuarioLocalizacao);
+                }
+
+                responseMessage = "Operação concluída";
+                log.LogInformation("Operação concluída sem exceções - realizando commit . . .");
+                session.CommitTransaction();
+            }
+            catch (Exception e)
+            {
+                session.AbortTransaction();
+                return new OkObjectResult("Erro: " + e.Message);
+                throw;
+            }
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
